8edfa2fdf337c17b25e62e3b69403e0b2b97b104
Matt Stone <matthew.stone12@gmail.com>
2015-03-05 13:26:12 -0500

    FSM definition, rough draft of Agent class, start on Zone animation

--- Zone.py
+++ Zone.py
@@ -1,35 +1,50 @@
+#!/usr/bin/env python
+
 import copy
 import itertools
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import agent
+
+
+class Box:
+    def __init__(self):
+        self.agents = []
+        self.signals = []
 
 class Zone:
     def __init__(self, width, height, cell_count):
         self.width = width
         self.height = height
         self.cell_count = cell_count
-        self.grid = [[[] for i in range(height)] for i in range(width)]
+        self.grid = [[Box() for i in range(height)] for i in range(width)]
+        self.display_grid = [[0 for i in range(height)] for i in range(width)]
 
     def populate(self):
         # Initialize cells in this zone
         boxes = list(
-            itertools.product(range(self.width),range(self.height)))
+            itertools.product(range(self.width), range(self.height)))
         random.shuffle(boxes)
         agent_boxes = boxes[:self.cell_count]
-        for (x,y) in agent_boxes:
-            self.grid[x][y].append(agent.Agent())
+        for (x, y) in agent_boxes:
+            self.grid[x][y].agents.append(agent.Agent())
+            self.display_grid[x][y] = 1
 
+    def update(self):
+        pass
+
+    # rename to animate
     def plot(self):
         fig, ax = plt.subplots()
-        agent_colors = {1:'b'}
+        agent_colors = {1: 'b'}
 
         print(self.grid)
         for (i, column) in enumerate(self.grid):
             for (j, box) in enumerate(column):
-                if len(box) > 0:
-                    print((i,j))
+                if len(box.agents) > 0:
+                    print((i, j))
                     ax.scatter(
                         i+.5, j+0.5,
                         color=agent_colors[1])
@@ -40,6 +55,48 @@ class Zone:
         ax.set_xticks([])
         ax.set_yticks([])
         plt.savefig("test")
+
+    def animate(self):
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0, self.width), ylim=(0, self.height))
+        print self.display_grid
+        mat = ax.matshow(self.display_grid)
+
+        def anim_update(tick):
+            self.update()
+            mat.set_data(self.display_grid)
+            return mat,
+
+        anim = animation.FuncAnimation(fig, anim_update, frames=200,
+                                       interval=20)
+        anim.save('test.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+        plt.show()
+
+    # def show(self):
+    #
+    #     scatter = ax.scatter([], [])
+
+    #     def init():
+    #         scatter.set_paths([], [])
+    #         return scatter,
+
+    #     def animate(tick):
+    #         agent_colors = {1: 'b'}
+    #         for (i, column) in enumerate(self.grid):
+    #             for (j, box) in enumerate(column):
+    #                 if len(box.agents) > 0:
+    #                     print((i, j))
+    #                     scatter.set_paths(
+    #                         i+.5, j+0.5,
+    #                         color=agent_colors[1])
+    #         return scatter,
+
+    #     anim = animation.FuncAnimation(fig, animate, init_func=init,
+    #                                    frames=200, interval=20, blit=True)
+    #     anim.save('test.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+    #     plt.show()
+
 
 def main():
     zone = Zone(6, 5, 3)
--- agent.py
+++ agent.py
@@ -1,3 +1,96 @@
+import random
+
+class Signal:
+    pass
+
+
+class SignalTracker:
+    def __init__(self):
+        self.signals = {
+            'CK1': 0,
+            'CK2': 0
+        }
+        self.agents = {
+            'DC1': False,
+            'DC2': False,
+        }
+
+    def reset(self):
+        for signal in self.signals:
+            self.signals[signal] = 0
+        for agent in self.agents:
+            self.agents[agent] = False
+
+
+class FSM:
+    """
+
+    Attributes
+    ----------
+    init_state : int
+    states : list (int)
+    transitions : list (int, int, dict, dict)
+        (src, dst, signals, agents)
+
+    """
+    def __init__(self, init_state, states, transitions):
+        self.current = init_state
+        self.states = states
+        self.transitions = transitions
+
+    def update(self, tracker):
+        passed = []
+        for tn in transitions:
+            if tn[0] == self.current:
+                signals_passed = True
+                for signal in tn[2].keys():
+                    if tracker[signal] <= tn[2][signal]:
+                        signals_passed = False
+
+                agents_passed = True
+                for agent in tn[3].keys():
+                    if tracker[agent] != tn[3][agent]:
+                        agents_passed = False
+
+                if signals_passed and agents_passed:
+                    passed.append(tn[1])
+
+        if len(passed) > 1:
+            self.current = passed(random.randint(0, len(passed) - 1))
+        elif len(passed) == 1:
+            self.current = passed[0]
+
+
 class Agent:
-    def __init__(self, probe_range = 1):
+    def __init__(self, fsm=None, probe_range=1):
+        self.fsm = fsm
+        self.state = None  # TODO: init state
         self.probe_range = probe_range
+        self.tracker = SignalTracker()
+        self.signals = {} # Dict {state: (signal, value)}
+        self.kind = ""
+        # self.life
+        # self.duration
+
+    def probe(self, boxes):
+        for box in boxes:
+            for agent in box.agents:
+                self.tracker[agent.kind] = True
+            for signal in box.signals:
+                self.tracker[signal.kind] += signal.value
+
+        # TODO: calculate movment
+        # Return number of cells to move horiz/vert
+        x = -1
+        y = 1
+        return x, y
+
+    def update(self):
+        # update fsm
+        # emit signal based on new tpe
+        self.fsm.update(self.tracker)
+
+        # return signals to emit in Zone
+        signal = []
+        return signal
+
